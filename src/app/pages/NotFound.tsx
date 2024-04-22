35460494b47245669f35e3015f00d95bcce3d197
roberto2002-03-12 <robertog-18@hotmail.com>
2024-04-21 22:34:20 -0500

    added style to not found

--- src/app/pages/NotFound.tsx
+++ src/app/pages/NotFound.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
+
   return (
-    <div>NotFound</div>
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <h2 className="text-3xl font-medium">Not found</h2>
+      <Link to={'/'} className='text-sky-500 text-lg'>
+      Go home
+      </Link>
+    </div>
   )
 }
